b42b46833d6c8c8729eb7b8d622fc3e11e0a7505
Pranav Vashi <neobuddy89@gmail.com>
2020-04-10 13:12:56 +0530

    phoenix: Fix bluetooth A2DP support
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I41a3740932950871e8f5147da37af0ad25a79897
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- product_prop.mk
+++ product_prop.mk
@@ -10,16 +10,18 @@ ro.adb.secure=0 \
 ro.secure=0 \
 ro.debuggable=1
 
-# Audio
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.bluetooth.a2dp_offload.supported=false \
-    persist.bluetooth.a2dp_offload.disabled=true \
-    vendor.audio.feature.a2dp_offload.enable=false \
-    persist.vendor.qcom.bluetooth.enable.splita2dp=false
-
 # Bluetooth
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.bluetooth.library_name=libbluetooth.so
+    persist.bluetooth.bqr.event_mask=14  \
+    persist.bluetooth.bqr.min_interval_ms=500  \
+    vendor.bluetooth.soc=cherokee  \
+    vendor.qcom.bluetooth.soc=cherokee  \
+    persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac  \
+    persist.bluetooth.a2dp_offload.disabled=false  \
+    persist.vendor.bt.aac_frm_ctl.enabled=true  \
+    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptive  \
+    persist.vendor.qcom.bluetooth.enable.splita2dp=true  \
+    ro.bluetooth.a2dp_offload.supported=true
 
 # VoLTE
 PRODUCT_PRODUCT_PROPERTIES += \
