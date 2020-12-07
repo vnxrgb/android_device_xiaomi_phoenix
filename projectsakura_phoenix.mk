b82b6597cfd7b97f5fcfbe300d302883a3ea397c
vnxrgb <53448733+vnxrgb@users.noreply.github.com>
2020-12-08 04:53:27 +0530

    Update and rename havoc_phoenix.mk to projectsakura_phoenix.mk

--- projectsakura_phoenix.mk
+++ projectsakura_phoenix.mk
@@ -1,23 +1,23 @@
 #
-# Copyright (C) 2020 The havocOS Project
+# 
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common havoc stuff.
+# Inherit some common projectsakura stuff.
 $(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # official tag
-export export HAVOC_BUILD_TYPE=Official
+export export projectsakura_BUILD_TYPE=UnOfficial
 
 # maintainer
 PRODUCT_PROPERTIES_OVERRIDES += \
-	ro.havoc.maintainer = "Blacksuan19"
+	ro.havoc.maintainer = "vnxpotato"
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_phoenix
+PRODUCT_NAME := projectsakura_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
